fa3e544bb3b3a87ced1b1e4908a41b03a6a58238
misszhao1121 <542275297@qq.com>
2018-03-22 21:28:32 +0800

    05-if-elseif.py

--- if-else.py
+++
@@ -1,8 +0,0 @@
-you = input("你去吗")
-
-yourWife = input("你老婆去吗")
-#if you == "去" and yourWife == "去":
-if you == "去" or  yourWife == "不去":       
-        print("可以去做羞羞的事情了")
-else:
-        print("yes you can go to")
