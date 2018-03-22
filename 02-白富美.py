03ec050278e279561332c87d0b6acab2ac1b0d28
xwp <xwp_fullstack@163.com>
2018-03-22 17:33:37 +0800

    代码

---
+++ 02-白富美.py
@@ -0,0 +1,9 @@
+color = input("你白吗？")
+money = input("你有钱吗？")
+beautiful = input("你长得漂亮吗？")
+
+if color == "白" and money > "10000" and beautiful == "漂亮":
+    print("你是一个白富美")
+#python是通过代码快来确定语句范围的
+else:
+    print("你还需要一些成长才能成为白富美")
---
+++ 03-if逻辑运算符.py
@@ -0,0 +1,7 @@
+a = 30 
+
+if not (a > 0 and a <=50):
+
+    print("0-50之间")
+else:
+    print("不在50之间")
